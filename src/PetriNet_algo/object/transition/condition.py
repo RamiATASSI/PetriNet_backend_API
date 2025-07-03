c52d1aefdcafcc7992c729f0dd51fbe0cf8d920b
André Peiry <andre.peiry@epfl.ch>
2025-07-03 14:58:50 +0200

    [dev] Wrote most of condition (-List, -Switch)

--- src/PetriNet_algo/object/transition/condition.py
+++ src/PetriNet_algo/object/transition/condition.py
@@ -1,9 +1,15 @@
+import copy
+import random
 from typing import Callable
 
+from PetriNet_algo.data_structure.dict_list_builder import DictListBuilder
+from PetriNet_algo.object.place import Place
 from src.PetriNet_algo.object.token import Token
 from src.PetriNet_algo.object.transition.operator import OperatorId, PlaceId
 
 type ConditionId = int
+type TokenMap = dict[PlaceId, list[Token]]
+type OptionalTokenMap = None | TokenMap
 
 ########################################################################################################################
 ####################################################### Condition ######################################################
@@ -13,12 +19,13 @@ class Condition:
     """
     Attributes
     ----------
+    TODO Update
     condition_id : ConditionId
         The ID of the condition in the transition graph.
     condition : Callable[[PlaceId], (bool, list[Token])]
         A function that evaluate if the condition is true. The reason for the tuple is that some condition do not need
         tokens (or need a place to be empty). In any case, if the bool is false, the list[Token] is discarded.
-    place_dependencies : PlaceId
+    place_dependencies : list[Place]
         The place into which the condition depends.
     fetch_order: list[Token] -> list[Token]
         An ordering function that will select the token to fetch (for example, the ones who where there first, the
@@ -28,11 +35,11 @@ class Condition:
     """
     def __init__(self,
                  condition_id: ConditionId,
-                 place_dependencies: list[PlaceId],
+                 place_dependencies: list[Place],
                  operator_dest: list[OperatorId],
-                 consumption_condition: Callable[[dict[PlaceId, list[Token]]], (bool, list[Token])],
-                 fetch_order: Callable[[list[Token]], list[Token]],
-                 additional_condition: Callable[[dict[PlaceId, list[Token]]], bool] = lambda _: True
+                 consumption_condition: Callable[[TokenMap], OptionalTokenMap],
+                 fetch_order: Callable[[TokenMap], TokenMap],
+                 additional_condition: Callable[[TokenMap], bool] = lambda _: True
              ):
         self.condition_id = condition_id
         self.place_dependencies = place_dependencies
@@ -41,17 +48,25 @@ class Condition:
         self.additional_condition = additional_condition
         self.fetch_order = fetch_order
 
-    def fetch_tokens_copy(self) -> dict[PlaceId, list[Token]]:
-        # TODO
-        ...
+    def fetch_tokens_copy(self) -> TokenMap:
+        """
+        Fetches all tokens from the place_dependencies (even ones that aren't needed for the condition).
+        :return:
+            TODO
+        """
+        token_map = DictListBuilder[PlaceId, Token]()
+        for place in self.place_dependencies:
+            token_map.append_all(place.place_name, place.get_tokens())
+        return token_map.build()
 
-    def is_condition_satisfiable(self) -> bool:
+    def is_condition_satisfiable(self, tokens: TokenMap) -> OptionalTokenMap:
         """
-        Look if the place on which this condition depends owns the necessary tokens to satisfy the condition.
-        :return: `True` if the condition is satisfiable, `False` otherwise.
+        TODO
         """
-        # TODO Update
-        return self.condition(self.place_id_dependency)[0]
+        if self.additional_condition(tokens):
+            return self.consumption_condition(self.fetch_order(tokens))
+        else:
+            return None
 
 
 ########################################################################################################################
@@ -61,27 +76,47 @@ class ConditionList:
     def __init__(self, conditions: list[Condition]):
         self.conditions = conditions
 
-    def is_condition_list_satisfiable(self, tokens: dict[PlaceId, list[Token]]) -> bool:
-        # TODO
-        ...
+    def is_condition_list_satisfiable(self) -> OptionalTokenMap:
+        # Generating (Fetching) available tokens
+        tokens = DictListBuilder()
+        for condition in self.conditions:
+            tokens.append_map(condition.fetch_tokens_copy())
+        tokens = tokens.build()
 
-    def fetch_tokens_copy(self) -> dict[PlaceId, list[Token]]:
-        # TODO
-        ...
+        # Checking conditions
+        tokens_left = DictListBuilder()
+        tokens_left.append_map(tokens)
+        tokens_used = DictListBuilder()
+        for condition in self.conditions:
+            tokens_for_condition = condition.is_condition_satisfiable(tokens_left.build())
+            if tokens_for_condition is not None:
+                tokens_left.remove_map(tokens_for_condition)
+                tokens_used.append_map(tokens_for_condition)
+            else:
+                return None
+        return tokens_used.build()
+
+    def __fetch_tokens_copy__(self) -> TokenMap:
+        tokens_map = DictListBuilder()
+        for condition in self.conditions:
+            tokens_map.append_map(condition.fetch_tokens_copy())
+        return tokens_map.build()
 
 ########################################################################################################################
 ################################################### Condition Switch ###################################################
 ########################################################################################################################
 class ConditionSwitch:
-    def __init__(self, condition_list: list[ConditionList], is_priority_ordered: bool):
-        self.condition_list = condition_list
+    def __init__(self, condition_lists: list[ConditionList], is_priority_ordered: bool):
+        self.condition_lists = condition_lists
         self.is_priority_ordered = is_priority_ordered
 
-    #  is_satisfiable()
-    def is_condition_switch_satisfiable(self):
-        # TODO
-        ...
+    def is_condition_switch_satisfiable(self) -> OptionalTokenMap:
+        if not self.is_priority_ordered:
+            # We don't care that the original list is lost
+            random.shuffle(self.condition_lists)
 
-    def __fetch_tokens_copy__(self):
-        # TODO
-        ...
+        for condition_list in self.condition_lists:
+            r = condition_list.is_condition_list_satisfiable()
+            if r is not None:
+                return r
+        return None
