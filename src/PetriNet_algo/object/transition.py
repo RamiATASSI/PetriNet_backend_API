c357b60e85bef34956781c369463a38be881cac0
André Peiry <andre.peiry@epfl.ch>
2025-07-03 17:21:42 +0200

    [dev] Small work on Transition#consume

--- src/PetriNet_algo/object/transition.py
+++ src/PetriNet_algo/object/transition.py
@@ -1,15 +1,19 @@
-from src.PetriNet_algo.data_structure.dict_list_builder import DictListBuilder
+"""TODO"""
+from PetriNet_algo.object.transition.condition import OptionalTokenMap
+from PetriNet_algo.petriNet import PlaceDict
 from src.PetriNet_algo.object.place import Place
-from src.PetriNet_algo.object.token import Token
-from src.PetriNet_algo.object.transition.condition import ConditionSwitch
+from src.PetriNet_algo.object.transition.condition import ConditionSwitch, TokenMap
 from src.PetriNet_algo.object.transition.operator import OperatorGraph
 
 
 class Transition:
+    """TODO"""
     def __init__(self,
-                 transition_name,
+                 transition_name: str,
                  condition_switch: ConditionSwitch,
-                 operator_graph: OperatorGraph
+                 operator_graph: OperatorGraph,
+                 triggering_event: str = "True",
+                 duration: int = 0
              ):
         # Info
         self.transition_name: str = transition_name
@@ -17,22 +21,23 @@ class Transition:
         # Pipeline
         self.condition_switch = condition_switch
         self.operator_graph= operator_graph
-        self.consumed_tokens = list()
+        self.consumed_tokens = []
 
         # Miscellaneous
-        self.triggering_event: str = ...
-        self.duration = ...
-        self.is_sensitized = False
-        self.is_triggered = False
+        self.triggering_event: str = triggering_event
+        self.duration = duration
+        self.is_sensitized: bool = False
+        self.is_triggered: bool = False
 
     def check_sensitization(self) -> bool:
         """
         TODO
         :return:
         """
-        self.is_sensitized = self.condition_switch.is_condition_switch_satisfiable()
+        self.is_sensitized = self.condition_switch.is_condition_switch_satisfiable() is None
         return self.is_sensitized
 
+    @DeprecationWarning
     def shortcut_trigger_if_sensitized(self):
         """
         TODO
@@ -43,51 +48,31 @@ class Transition:
             return True
         return False
 
-    def consume_tokens(self) -> dict[Place, list[Token]]:
+    def consume_tokens(self, places: PlaceDict) -> OptionalTokenMap:
         """
         Consume tokens that satisfy the conditions of the transition.
-        :return: The consumed tokens.
-        """
-        dict_list_builder = DictListBuilder[Place, Token]()
-        for condition in self.conditions:
-            eaten_tokens: list[Token] = condition.eat_tokens() # Get eaten Token
-            dict_list_builder.append_all(condition.place, eaten_tokens) # Add to return value
-            for eaten_token in eaten_tokens:
-                condition.place.remove_tokens(eaten_token.token_type,)
-
-        return dict_list_builder.build()
-
-        # OLD
-        #deleted_colors = {}
-        ##emit('message', f"Consuming tokens for transition {self.transition_name}")
-        #for place, token in self.token_consumption.items():
-        #    for color, weight in token.items():
-        #        if weight > 0:
-        #            place.add_colored_tokens(color, -weight)
-        #            if place not in deleted_colors:
-        #                deleted_colors[place] = set()
-        #            deleted_colors[place].add(color)
-        #return deleted_colors
-
-    def produce_tokens(self) -> dict[Place, set[str]]:
-        """
-        TODO
         :return:
+            The consumed tokens.
         """
-        # TODO
+        tokens_to_consume = self.condition_switch.is_condition_switch_satisfiable()
+        if tokens_to_consume is None:
+            return None
+
+        for place_id in tokens_to_consume:
+            for token in tokens_to_consume[place_id]:
+                places[place_id].remove(token) # TODO Remove particular token
+
+        self.consumed_tokens = tokens_to_consume
+        return tokens_to_consume
+
+    def produce_tokens(self) -> TokenMap:
+        """
+        Produce the tokens of a transition.
+        :return:
+            The produced tokens.
+        """
+        # TODO Run the OperatorGraph
         pass
 
-        # OLD
-        #added_colors = {}
-        ##emit('message', f"Producing tokens for transition {self.transition_name}")
-        #for place, token in self.token_production.items():
-        #    for color, weight in token.items():
-        #        if weight > 0:
-        #            place.add_colored_tokens(color, weight)
-        #            if place not in added_colors:
-        #                added_colors[place] = set()
-        #            added_colors[place].add(color)
-        #return added_colors
-
     def __str__(self) -> str:
-        return f"{self.transition_name}(used_map_during_consumption={self.used_map_during_consumption}, consumed_tokens={self.consumed_tokens})"
+        return f"{self.transition_name}(consumed_tokens={self.consumed_tokens})"
